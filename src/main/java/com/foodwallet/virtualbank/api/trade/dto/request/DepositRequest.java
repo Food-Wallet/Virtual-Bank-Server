1a980425917bd37bb8c10eab846e2e699f82adbd
Chaos0103 <lyt1228@naver.com>
2024-01-15 15:44:52 +0900

    feat: 계좌 이체 API 스켈레톤 코드 구현

---
+++ src/main/java/com/foodwallet/virtualbank/api/trade/dto/request/DepositRequest.java
@@ -0,0 +1,7 @@
+package com.foodwallet.virtualbank.api.trade.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class DepositRequest {
+}
---
+++ src/main/java/com/foodwallet/virtualbank/api/trade/dto/response/DepositResponse.java
@@ -0,0 +1,7 @@
+package com.foodwallet.virtualbank.api.trade.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class DepositResponse {
+}
